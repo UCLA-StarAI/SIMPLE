bd7199c012a3bbb29a2ebaa5fa82b057be2aaae0
zhezengzz <zhezeng@g.ucla.edu>
2022-10-31 16:40:21 -0700

    add L2X scripts

---
+++ L2X/data/README.md
@@ -0,0 +1,1 @@
+Place the uncompressed files from http://people.csail.mit.edu/taolei/beer/ here.
---
+++ L2X/run.sh
@@ -0,0 +1,61 @@
+mkdir log
+python3 L2X-SIMPLE.py --seed 0 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 1 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 2 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 3 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 4 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 5 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 6 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 7 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 8 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 9 --gpu 0 --lr 0.005 --aspect 0 --k 10
+python3 L2X-SIMPLE.py --seed 0 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 1 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 2 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 3 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 4 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 5 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 6 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 7 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 8 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 9 --gpu 0 --lr 0.001 --aspect 1 --k 5
+python3 L2X-SIMPLE.py --seed 0 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 1 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 2 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 3 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 4 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 5 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 6 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 7 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 8 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 9 --gpu 0 --lr 0.0005 --aspect 1 --k 10
+python3 L2X-SIMPLE.py --seed 0 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 1 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 2 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 3 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 4 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 5 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 6 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 7 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 8 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 9 --gpu 0 --lr 0.0001 --aspect 1 --k 15 --clip
+python3 L2X-SIMPLE.py --seed 0 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 1 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 2 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 3 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 4 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 5 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 6 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 7 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 8 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 9 --gpu 0 --lr 0.001 --aspect 2 --k 10
+python3 L2X-SIMPLE.py --seed 0 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 1 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 2 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 3 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 4 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 5 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 6 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 7 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 8 --gpu 0 --lr 0.0005 --aspect 3 --k 10
+python3 L2X-SIMPLE.py --seed 9 --gpu 0 --lr 0.0005 --aspect 3 --k 10
